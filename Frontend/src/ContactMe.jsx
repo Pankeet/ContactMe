dfdecfcc81ae6e2ecc3efe2b906088a80861d9f1
Pankeet <pankeet04@gmail.com>
2025-06-13 18:09:16 +0530

    Added Responsiveness for Phone Screen Size

--- Frontend/src/ContactMe.jsx
+++ Frontend/src/ContactMe.jsx
@@ -56,46 +56,46 @@ export default function Contact(){
         }   
     }
     return (
-        <div className="font-serif overflow-auto">
-            <div className="text-5xl font-light pl-10 pt-3 pb-3 bg-[url('/banner.png')] bg-cover bg-no-repeat ">
+        <div className="font-serif ">
+            <div className="sm:text-2xl md:text-3xl lg:text-5xl font-light px-5 py-3 bg-[url('/banner.png')] bg-cover bg-no-repeat w-full text-xl">
                 <div>Contact</div>
             </div>
-            <div className="pt-10 pl-10 flex">
-                <span className="text-5xl">Helpful <br />Information</span>
-                <span className="text-5xl pl-60">General <br />Inquiries</span>
-                <span className="text-md pl-52 pt-10"> For general inquiries please , please <i><u className="cursor-pointer" onClick={fillform}> <br /> fill out this form to reach me</u> </i>
-                </span> 
+            <div className="pt-10 sm:pl-10 sm:flex">
+                <div className="sm:text-2xl md:text-3xl md:font-medium lg:text-5xl">Helpful <br />Information</div>
+                <div className="sm:text-2xl md:text-3xl md:font-medium lg:text-5xl sm:pl-60 pt-3">General <br />Inquiries</div>
+                <div className="text-md sm:pl-52 sm:pt-10 pt-3"> For general inquiries please , please <i><u className="cursor-pointer" onClick={fillform}> <br /> fill out this form to reach me</u> </i>
+                </div> 
             </div>
-            <div className="flex pt-20 pl-10">
-                <span className="text-2xl">Phone <br />
-                <span className="text-xl cursor-pointer" onClick={() => CopyText('9875142251', true)}>+91 9875142251</span>
+            <div className="sm:flex sm:pt-20 pt-4 sm:pl-10 ">
+                <span className="sm:text-lg md:text-xl lg:text-2xl ">Phone <br />
+                <span className="sm:text-md md:text-lg lg:text-xl cursor-pointer" onClick={() => CopyText('9875142251', true)}>+91 9875142251</span>
                 </span>
-                <span className="pl-[23rem] pt-4 text-xl">First name<span className='text-red-900'>*</span> <br />
+                <span className="sm:pl-[23rem] pt-4 md:text-xl text-lg flex sm:block">First name<span className='text-red-900'>*</span> <br />
                 <input type="text" ref={formRef} className={`pt-1 outline-none ${ focusoninp ? 'border-2 border-red-500 rounded-md': 'border-gray-600 border-b'}`}></input>
                 </span>
-                <span className="pl-48 pt-3.5 text-xl">Last name<span className='text-red-900'>*</span> <br />
+                <span className="sm:pl-48 pt-3.5 md:text-xl text-lg flex sm:block">Last name<span className='text-red-900'>*</span> <br />
                 <input type="text" ref={lname} className="border-gray-600 border-b-1 pt-1 outline-none"></input>
                 </span>
             </div>
-            <div className="flex pt-10 pl-10">
-            <span className="text-2xl">Email <br />
-                <span className="text-xl cursor-pointer" onClick={() => CopyText('pankeet04@gmail.com' , false)}>pankeet04@gmail.com</span>
+            <div className="sm:flex pt-10 sm:pl-10">
+            <span className="sm:text-lg md:text-xl lg:text-2xl">Email <br />
+                <span className="sm:text-md md:text-lg lg:text-xl cursor-pointer" onClick={() => CopyText('pankeet04@gmail.com' , false)}>pankeet04@gmail.com</span>
                 </span>
-                <span className='pl-[307px] pt-4 text-xl'>Email<span className='text-red-900'>*</span> <br />
+                <span className='sm:pl-[307px] pt-4 md:text-xl text-lg flex sm:block'>Email<span className='text-red-900'>*</span> <br />
                 <input type="email" ref={user} className="border-gray-600 border-b-1 pt-1 outline-none"></input>
                 </span>
-                <span className='pl-48 pt-3.5 text-xl'>Subject<span className='text-red-900'>*</span> <br />
+                <span className='sm:pl-48 pt-3.5 md:text-xl text-lg flex sm:block'>Subject<span className='text-red-900'>*</span> <br />
                 <input type="text" ref={sub} className="border-gray-600 overflow-none border-b-1 pt-1 outline-none w-2xs"></input>
                 </span>
             </div>
-            <div className="flex pt-10 pl-10">
-                <span className="text-2xl">Location <br /> <span className="text-xl">Vadodara, Gujarat, India</span></span>
-                <span className="pl-[290px] pt-4 text-xl">Message<span className='text-red-900'>*</span> <br />
+            <div className="sm:flex pt-10 sm:pl-10">
+                <span className="sm:text-lg md:text-xl lg:text-2xl">Location <br /> <span className="md:text-xl text-md">Vadodara, Gujarat, India</span></span>
+                <span className="sm:pl-[290px] pt-4 md:text-xl text-lg flex sm:block">Message<span className='text-red-900'>*</span> <br />
                 <input type="text" ref={msg} className="border-gray-600 border-b-1 pt-1 outline-none w-2xl"></input>
                 </span>
             </div>
 
-            <button type="submit" disabled={loading} onClick={sendMail} className="mt-10 ml-[51rem] px-6 py-2 bg-black text-white text-lg rounded hover:bg-white transition-colors duration-300 hover:text-black">{loading ? "Sending Mail..." : "Send Mail"}</button>
+            <button type="submit" disabled={loading} onClick={sendMail} className="ml-48 mt-10 sm:ml-[51rem] px-6 py-2 mb-5 bg-black text-white text-lg rounded hover:bg-white transition-colors duration-300 hover:text-black">{loading ? "Sending Mail..." : "Send Mail"}</button>
             
             <ToastContainer position="top-center" autoClose={2000} transition={Slide} pauseOnHover={false}/>
         </div>
